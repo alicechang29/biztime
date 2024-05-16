ee327a4c714c783ba4aaf942ffdb415722ad40cd
Ricardo Perez <rxperez12@gmail.com>
2024-05-16 12:34:41 -0700

    finished companies post, put routes

--- routes/companies.js
+++ routes/companies.js
@@ -6,6 +6,7 @@ import { BadRequestError } from "../expressError.js";
 
 const router = express.Router();
 
+/** GET / - returns `{companies: [{code, name}, ...]}` */
 router.get("",
   async function (req, res, next) {
 
@@ -16,9 +17,11 @@ router.get("",
     return res.json({ companies });
   });
 
+/** GET /[code] - return data about one company:
+ * `{company: {code, name, description}}` */
+
 router.get("/:code",
   async function (req, res, next) {
-    if (req.body === undefined) throw new BadRequestError();
     const code = req.params.code;
 
     const results = await db.query(
@@ -29,5 +32,37 @@ router.get("/:code",
     return res.json({ company });
   });
 
+
+router.post('', async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+  const { code, name, description } = req.body;
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+     VALUES ($1, $2, $3) RETURNING code, name, description`,
+    [code, name, description]
+  );
+
+  const newCompany = result.rows[0];
+  return res.status(201).json({ company: newCompany });
+});
+
+router.put('/:code', async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+
+  const code = req.params.code;
+  const { name, description } = req.body;
+
+  const result = await db.query(
+    `UPDATE companies
+     SET name=$1, description=$2
+     WHERE code = $3 RETURNING code, name, description`,
+    [name, description, code]
+  );
+
+  const changedCompany = result.rows[0];
+  if (!changedCompany) throw new NotFoundError(`No matching company: ${code}`);
+
+  return res.json({ company: changedCompany });
+});
 
 export default router;
